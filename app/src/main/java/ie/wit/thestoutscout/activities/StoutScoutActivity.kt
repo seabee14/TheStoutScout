a381de94f87c2ef3c48112db7fdf5ed618c4e72b
Conor Brennan <conorbren@gmail.com>
2023-03-23 17:29:56 +0000

    Added ArrayList allowing the user to add multiple pubs

--- app/src/main/java/ie/wit/thestoutscout/activities/StoutScoutActivity.kt
+++ app/src/main/java/ie/wit/thestoutscout/activities/StoutScoutActivity.kt
@@ -14,21 +14,22 @@ class StoutScoutActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityStoutscoutBinding
     var pub = PubModel()
+    val pubs = ArrayList<PubModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityStoutscoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         Timber.plant(Timber.DebugTree())
-
         i("Stout Scout Activity started...")
 
         binding.btnAdd.setOnClickListener() {
             pub.title = binding.stoutscoutTitle.text.toString()
             if (pub.title.isNotEmpty()) {
-                i("add Button Pressed: ${pub.title}")
+                pubs.add(pub.copy())
+                i("add Button Pressed: ${pub}")
+                for (i in pubs.indices)
+                { i("Pub[$i]:${this.pubs[i]}") }
             } else {
                 Snackbar
                     .make(it, "Please Enter a title", Snackbar.LENGTH_LONG)
